e99bfb1f480a90c48861859b3284e9fe0e595ca9
Delyan Damyanov <damyanov.delyan@gmail.com>
2019-06-09 20:12:41 +0300

    Add files via upload

---
+++ houseOfCats.js
@@ -0,0 +1,20 @@
+// data validation inside the main function - code will execute after checking if data is VALID using try...catch
+
+function houseOfCats(legs) {
+  try {
+    if (isNaN(legs)) throw "not a number";
+    if (legs >= 50) throw "too high";
+    if (legs < 0) throw "too low";
+    if (legs % 2 !== 0) throw "odd number";
+  } catch (err) {
+    console.log(`Input is ${err}`);
+  }
+  const maxPossiblePeople = legs / 2;
+  let result = [];
+  for (let i = 0; i <= maxPossiblePeople; i++) {
+    if ((legs - i * 2) % 4 === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
